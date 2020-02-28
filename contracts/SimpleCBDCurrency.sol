cb70ae99cafb933f3b28782c3ab3572cf58efff7
Jamiel Sheikh <i@jamiel.net>
2020-02-27 23:17:55 -0500

    updates

--- contracts/SimpleCBDCurrency.sol
+++ contracts/SimpleCBDCurrency.sol
@@ -21,4 +21,13 @@ contract SimpleCBDCurrency {
         _balances[centralbank] = _balances[centralbank] + amount;
     }
 
+    function transfer(address receiver, uint256 amount) public {
+        _balances[receiver] = _balances[receiver] + amount;
+        _balances[centralbank] _balances[centralbank] - amount;
+    }
+
+    function balance(address account) public {
+        return _balances[account];
+    }
+
 }
